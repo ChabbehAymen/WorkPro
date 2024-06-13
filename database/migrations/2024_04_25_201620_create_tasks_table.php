4d87ef0c1333b7f046efc981448a19cc5b30b2e1
aymen <devaymen15.5.3@gmail.com>
2024-06-13 04:53:16 +0100

    updated taks migratino

--- database/migrations/2024_04_25_201620_create_tasks_table.php
+++ database/migrations/2024_04_25_201620_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->integer('status')->default(0);
             $table->string('img')->default('none');
             $table->foreignId('project_id')->constrained();
